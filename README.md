3022d8f43067c40c6942f287e26f2bcb7ffa36b1
WilliamLHostetter <William.L.Hostetter@gmail.com>
2025-03-31 12:29:24 -0700

    1st Commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Square root to n digits of precision
+Python implementations of numerical methods to find the square root of an input floating point number to an arbitrary precision.
+
+Includes a GUI dialog box to enter the input number and precision. 
+<p align="center" width="100%">
+  <img style="display: block; margin: auto;" src="./images/input-window.png"/>
+</p>
+<br>  
+The output is displayed in scrollable text box that can be highlighted and copied.  
+
+<br>
+<p align="center" width="100%">
+  <img style="display: block; margin: auto;" src="./images/output-window.png"/>
+</p>
+<br>
+
+## Table of Contents
+- [Prerequisites](#prerequisites-heading)
+- [Dependencies](#dependencies-heading)
+- [Usage](#usage-heading)
+- [Bisection vs Newton–Raphson Methods](#bisection-vs-newton-raphson-heading)
+
+<a name="prerequisites-heading"></a>
+## Prerequisites
+Python 3 (tested in version 3.12.2)
+
+<a name="prerequisites-heading"></a>
+## Dependencies
+There are no external dependencies needed for these routines. They only use built-in Python modules and packages, such as the [tkinter](https://docs.python.org/3/library/tkinter.html) package and the [decimal](https://docs.python.org/3/library/decimal.html) module.
+
+<a name="usage-heading"></a>
+## Usage
+### sqrt_bisection.py
+The Bisection method can be ran directly in the console with  
+```console
+python sqrt_bisection.py
+```
+
+### sqrt_newton_raphson.py
+The Newton–Raphson method can be ran directly in the console with  
+```console
+python sqrt_newton_raphson.py
+```
+<a name="bisection-vs-newton-raphson-heading"></a>
+## Bisection vs Newton-Raphson Methods
+Newton's method is relatively faster than the Bisection method because the convergence rate of the Bisection method is linear and Newton's method is quadratic.  
+
+However, binary search is less sensitive to the initial guess and is suitable for functions where the derivative is not easily computable or when the function is not differentiable.
---
+++ sqrt_newton_raphson.py
@@ -0,0 +1,114 @@
+'''
+Python implementation of the Newton-Raphson method to find square root of an 
+input floating point number to an arbitrary precision.
+Includes a GUI dialog box to enter the input number and precision. The output is 
+displayed in scrollable text box that can be highlighted and copied. 
+'''
+
+import tkinter as tk
+from tkinter import scrolledtext, messagebox
+import decimal
+import time
+
+
+
+def squareRootNewtonRaphson(input_number, precision_num_digits):
+    '''Function to return the square root of a number using Newton-Raphson's method'''
+    decimal_precision = precision_num_digits + len(str(int(input_number))) + 1
+    decimal.getcontext().prec = decimal_precision
+    n = decimal.Decimal(input_number)
+    tolerance = decimal.Decimal(10) ** decimal.Decimal(-precision_num_digits)
+    # Initial guess of the sqrt(n) as n/2
+    estimate = n/decimal.Decimal(2)
+    # To count the number of iterations to report in output
+    iterations = 0
+    while True:
+        iterations += 1
+        # Calculate closer approximation of root
+        root = decimal.Decimal(0.5) * (decimal.Decimal(estimate) + (decimal.Decimal(n) / decimal.Decimal(estimate))) 
+        # Check for closeness 
+        if (abs(estimate - root) < tolerance):
+            break
+        # Refine initial estimate 
+        estimate = root
+    result = round(root, precision_num_digits)
+    return result, iterations
+
+
+def readInputsAndCompute(input_window, input1_var, input2_var):
+    input_x_str = input1_var.get()    
+    input_precision_n_digits_str = input2_var.get()
+    
+    try:
+        input_number = float(input_x_str)
+        assert input_number > 0
+        print("Input number =", input_number)
+    except Exception :
+        error_msg = "Enter a positive number for x"
+        messagebox.showerror("Input Error", error_msg)
+        return None
+    
+    try:
+        precision_num_digits = int(input_precision_n_digits_str)
+        assert precision_num_digits >= 0
+        print("Input precision number of digits =", precision_num_digits)
+    except Exception :
+        error_msg = "Enter a positive integer n for precision"
+        messagebox.showerror("Input Error", error_msg)
+        return None
+    
+    start_time = time.perf_counter()
+    # result = squareRootBinary(x, precision_num_digits)
+    result, iterations = squareRootNewtonRaphson(input_number, precision_num_digits)
+    end_time = time.perf_counter()
+    result_str = f"sqrt({input_number}) = {result:.{precision_num_digits}f}"
+    iterations_str = f"Number of Iterations = {iterations}"
+    print(result_str)
+
+    elapsed_time = end_time - start_time # in seconds
+    if elapsed_time >= 1.0:
+        elapsed_time_str = f"Computation time = {elapsed_time:.4f} s"
+    else:
+        elapsed_time_str = f"Computation time = {elapsed_time*1000:.4f} ms"
+    print(elapsed_time_str)
+
+    # output results to text window
+    results_window = tk.Toplevel(input_window)
+    results_window.title("Output")
+    scrolledText = scrolledtext.ScrolledText(results_window, width=60, height=8) # width and height units are number of characters
+    output_text_str = result_str + "\n\n" + elapsed_time_str + "\n" + iterations_str
+    scrolledText.insert(tk.INSERT, output_text_str)
+    scrolledText.pack(fill=tk.BOTH, expand=True)
+
+
+def main():
+
+    input_window = tk.Tk()
+    input_window.title("Input")
+    # input_window.geometry("350x175")
+    input_window.geometry("400x175")
+    input_window.eval('tk::PlaceWindow . center')
+
+    # declaring string variable for storing 2 inputs
+    input1_var=tk.StringVar()
+    input2_var=tk.StringVar()
+
+    tk.Label(input_window, text="Enter positive floating point number for sqrt()", font=("Segoe UI", 14)).grid(row=0)
+    tk.Label(input_window, text="Enter number of digits for precision", font=("Segoe UI", 14)).grid(row=2)
+    entry1 = tk.Entry(input_window, font=("Arial",14), textvariable = input1_var)
+    entry2 = tk.Entry(input_window, font=("Arial",14), textvariable = input2_var)
+    
+    input_window.columnconfigure(0, weight=1)
+    # input_window.columnconfigure(1, weight=1)
+    # input_window.columnconfigure(2, weight=1)
+    entry1.grid(row=1, column=0, sticky="nsew", padx=(10,10))
+    entry2.grid(row=3, column=0, sticky="nsew", padx=(10,10))
+
+    button = tk.Button(input_window, text="Submit", command=lambda: readInputsAndCompute(input_window, input1_var, input2_var))
+    button.grid(row=5,column=0, pady=(20, 20))
+
+    input_window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
